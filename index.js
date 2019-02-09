f2c1c859f26e6d9413f8e2a9e1b15de0787651dd
AndyGarey <elsa.garnica01@gmail.com>
2019-02-08 19:34:20 -0600

    Agregando historia de indes.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var cowsay = require('cowsay');
+
+let nombre = "Andy";
+let fraceVaquita = "Surcaré los 7 mares de pasto";
+
+let historia = "Hola, mi nombre es " + nombre + ". Y hoy me enconté con una vaquita pitara que decía: " + fraceVaquita;
+
+console.log(historia);
+
+console.log(cowsay.say({
+  text : fraceVaquita,
+  e : "x-o",
+  T : "v "
+}));
