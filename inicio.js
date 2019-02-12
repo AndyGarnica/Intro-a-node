374d1447296f7ed9604a3ea162838aec67bdfdb5
AndyGarey <elsa.garnica01@gmail.com>
2019-02-11 19:12:20 -0600

    agregando seccion de inicio

--- inicio.js
+++ inicio.js
@@ -1,5 +1,5 @@
 module.exports = {
     historia: function(personaje, lugar) {
-        return `Había una vez un niño que se llamaba ${personaje}  y el vivia en ${lugar}.`
+        return `Había una vez un niña que se llamaba ${personaje}  y ella vivia en ${lugar}.`
     }
 }
