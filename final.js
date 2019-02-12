5f4f26dccadc5f277f02185bec4452d73ded1f44
AndyGarey <elsa.garnica01@gmail.com>
2019-02-11 19:13:39 -0600

    agregando seccion de final

---
+++ final.js
@@ -0,0 +1,5 @@
+module.exports = {
+    historia: function(personaje, villano, hobby, lugar) {
+        return `Quién se enfureció muchisimo y pelearon a muerte ${personaje} derroto con facilidad a ${villano} quien no tuvo mas remedio que regresar la luz y ${personaje} volvio a ser feliz como siempre.`
+    }
+}
