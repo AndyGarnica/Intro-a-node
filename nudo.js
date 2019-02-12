6b5d914b130fdb9fc5412879d6f7a67241fb08de
AndyGarey <elsa.garnica01@gmail.com>
2019-02-11 19:13:01 -0600

    agregando seccion de nudo

---
+++ nudo.js
@@ -0,0 +1,5 @@
+module.exports = {
+    historia: function(personaje, villano, hobby, lugar) {
+        return `En donde le gustaba ${hobby} y disfrutar de la tranquilidad, una tarde su malo vecino ${villano} desconecto la luz del luga dejando si WIFI a ${personaje}.`
+    }
+}
